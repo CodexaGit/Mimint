0ffc68210cae98207a23087d96b581472768386b
Sebanev15 <116035791+Sebanev15@users.noreply.github.com>
2024-06-12 20:47:15 -0300

    eliminar directorio js para subir uno nuevo

--- js/registro.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const formulario = document.querySelector(".formulario");
-    setTimeout(function(){
-        formulario.classList.add("mostrar");
-    }, 2000);
-});
